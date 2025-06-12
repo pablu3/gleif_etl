4ba6892856bcc2f3993eedba95ab4e79560d5e88
Pablo <pawel-blukacz@wp.pl>
2025-06-12 10:36:09 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import requests as rq
+import warnings
+warnings.filterwarnings('ignore')
+
+raw_data = pd.read_csv("mail.csv", sep=";")
+
+# data required in the final database
+data = raw_data[["A_LEICODE", "OSNAZWA", "CENTKRAJKOD", "CENTKRAJNAZWA", "OSADRMIEJSC", "A_GRUPKAP"]]
+
+# inserting empty columns for data that will be extracted from the API
+scrapped_data = data.copy()
+scrapped_data['lei_s'] = ''
+scrapped_data['nazwa_s'] = ''
+scrapped_data['krajkod_s'] = ''
+scrapped_data['kodp_s'] = ''
+scrapped_data['direct_parent'] = ''
+scrapped_data['ultimate_parent'] = ''
